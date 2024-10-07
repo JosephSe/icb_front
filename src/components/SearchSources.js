544b28ba20095c269b9fac5e8ad0711aa84eca39
MumthasThasni <mumthasthasninkt@gmail.com>
2024-10-07 15:47:55 +0100

    SearchSource page been formatted as per gov uk

--- src/components/SearchSources.js
+++ src/components/SearchSources.js
@@ -1,28 +1,48 @@
 // src/components/SearchSources.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
-
-
+import { Button, Radio } from 'govuk-react';
 
 const SearchSources = () => {
   const navigate = useNavigate();
-  
+  const [selectedValue, setSelectedValue] = React.useState('');
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
   return (
-    <div className="container">
-      <h1>Search Sources</h1>
-      <h2>Do you know which sources you need to use for search?</h2>
-      <div className="radio-container">
-        <label className="radio-label">
-          <input type="radio" name="knowSources" value="yes" /> Yes
-        </label>
-        <label className="radio-label">
-          <input type="radio" name="knowSources" value="no" /> No
-        </label>
+    <div className="govuk-width-container">
+      <main className="govuk-main-wrapper" id="main-content" role="main">
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
+            <h1 className="govuk-heading-l">Search Sources</h1>
+            <h2 className="govuk-heading-m">Do you know which sources you need to use for search?</h2>
+
+            <div className="govuk-radios" data-module="govuk-radios">
+        <div className="govuk-radios__item">
+          <input className="govuk-radios__input" id="knowSourcesYes" name="knowSources" type="radio" value="yes" />
+          <label className="govuk-label govuk-radios__label" htmlFor="knowSourcesYes">
+            Yes
+          </label>
+        </div>
+        <div className="govuk-radios__item">
+          <input className="govuk-radios__input" id="knowSourcesNo" name="knowSources" type="radio" value="no" />
+          <label className="govuk-label govuk-radios__label" htmlFor="knowSourcesNo">
+            No
+          </label>
+        </div>
       </div>
-      <div className="button-container">
-        <button onClick={() => navigate('/search-source-choices')} className="button">Continue</button>
-      </div>
+
+            <Button
+              onClick={() => navigate('/search-source-choices')}
+               
+            >
+              Continue
+            </Button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
