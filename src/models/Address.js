ab205d46c675bb88403c870b914b3d767f529337
MumthasThasni <mumthasthasninkt@gmail.com>
2024-10-11 13:42:09 +0100

    Added Models

---
+++ src/models/Address.js
@@ -0,0 +1,11 @@
+class Address {
+    constructor(line1, line2, postcode, city) {
+      this.line1 = line1;
+      this.city = city;
+      this.postcode = postcode;
+      this.line2 = line2;
+    }
+  }
+  
+  export default Address;
+  
---
+++ src/models/BioDetails.js
@@ -0,0 +1,11 @@
+class BioDetails {
+    constructor(firstName, lastName,middleName, dateOfBirth) {
+      this.firstName = firstName;
+      this.middleName=middleName;
+      this.lastName = lastName;
+      this.dateOfBirth = dateOfBirth; // stored in 'DD-MM-YYYY' format
+    }
+  }
+  
+  export default BioDetails;
+  
